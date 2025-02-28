2e73a7fda2217568795d51891b483406684cfc8d
temwani nkowani <temwaninkowani@gmail.com>
2025-02-27 22:53:49 -0800

    Update index.js
    trying to fix white page

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Root from "./Root";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 
